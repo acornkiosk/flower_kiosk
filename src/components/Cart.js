ad215dedce974faf05f35704c6df292bc381450e
이름 <>
2024-03-07 22:22:46 +0900

    Modify: Cart.js 웹소켓으로 json 정보 전달하고 즉각 응답받을 수 있는 로직 드디어 파악함
    Cart.js 에 연습코드로 ws.send 함수에 json 전달하는 법 드디어 알아냄, 다만 코드 전체를 리팩토리해야될 것이 우려되어 주석으로 코드를 바꿀 필요가 있다는 내용만 기재해둠, 기존의 기능은 동일함
    
    Ref: #102

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,22 +13,35 @@ export default function Cart(props) {
   /** 웹소켓 연결관리 함수 */
   const connect = () => {
     /** 연결에 성공했을 경우 동작하는 메서드 */
-    ws.onopen = () => {
-      console.log("키오스크 : 실시간 화면연동 시작(웹소켓)")
-    }
+    ws.onopen = () => { console.log("키오스크 : 실시간 화면연동 시작(웹소켓)") }
     /** 연결과정에서 에러가 생겼을 때 동작하는 메서드 */
-    ws.onerror = () => {
-      alert("키오스크 : 화면 연동이 원활하게 이루어지지 않고 있습니다. 서버 확인이 필요합니다(웹소켓)")
-    }
+    ws.onerror = () => { alert("키오스크 : 화면 연동이 원활하게 이루어지지 않고 있습니다. 서버 확인이 필요합니다(웹소켓)") }
     /** 연결을 종료하고 싶을 때 동작하는 메서드 */
-    ws.onclose = () => {
-      console.log("키오스크 : 실시간 화면연동 종료(웹소켓)")
-    }
+    ws.onclose = () => { console.log("키오스크 : 실시간 화면연동 종료(웹소켓)") }
   }
   
-  const send = () => {
-    ws.send("보냄")
+  const send = (dto) => {
+    /** 연습 : 웹소켓에 보내게 될 정보들 */
+    var info = {
+      type : "ORDER_NEW",
+      id: dto.id,
+      is_completed: dto.is_completed,
+      kiosk_id: dto.kiosk_id,
+      menu_count: dto.menu_count,
+      menu_name: dto.menu_name,
+      menu_price: dto.menu_price,
+      options: dto.option,
+      order_id: dto.order_id,
+      regdate: dto.regdate
+    };
+    /** 전송 */
+    ws.send(JSON.stringify(info))
+
+    /** 리팩토링 대상 코드입니다.
+     * 현재 코드 : 단순히 연습코드를 통해 인지를 위한 신호용도로 사용중, flower_front 에서 받으면 refresh 함수를 실행하여 api 로 요청중
+     * 예정 : 서버로부터 이곳에 "/api/order/list" 을 요청하고 list 로 담은 다음에 ws.send() 할수로 보내보도록 하자. */
   }
+
   useEffect(() =>{
     /** 처음 주문 창에 들어오게되면 웹소켓에 연결을 시켜준다 */
     connect()
@@ -57,8 +70,10 @@ export default function Cart(props) {
     list.forEach(item =>{
       axios.post("/api/order", item)
       .then(res => {
+        console.log("주문하기 결과 : "+res.data.status)
         console.log(res.data)
-        send()
+        /** 웹소켓 전송 시작시점 */
+        send(res.data.dto)
       })
       .catch(erorr => console.log(erorr))
     })
