271c5882069488e6776519a5aeb2135887e9f65c
chan <oyc6739@gmail.com>
2024-03-12 17:53:58 +0900

    Add: "메뉴 상단 이미지디자인 수정"

--- src/components/HeaderImg.js
+++ src/components/HeaderImg.js
@@ -3,7 +3,7 @@ import { Image } from "react-bootstrap"
 export default function HeaderImg() {
   return (
 
-    <Image fluid src="/images/kiosk_header.jpg" style={{ width: '100%' }} />
+    <Image fluid src="/images/kiosk_header.png" style={{ width: '100%' }} />
 
   )
 }
